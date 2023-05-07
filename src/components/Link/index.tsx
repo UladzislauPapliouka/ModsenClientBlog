42b726055556fccc48724c1dd74ae14749fdbf1e
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2023-05-07 22:53:50 +0300

    feat: add underline to links

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,17 +1,21 @@
 import React, { type PropsWithChildren } from 'react';
 import NextLink, { type LinkProps } from 'next/link';
+import { useRouter } from 'next/router';
 import styles from './link.module.scss';
 
 const Link = ({
   href,
   children,
   ...restProps
-}: PropsWithChildren<LinkProps>) => (
-  <NextLink
-    href={href}
-    className={styles.link}
-    {...restProps}>
-    {children}
-  </NextLink>
-);
+}: PropsWithChildren<LinkProps>) => {
+  const router = useRouter();
+  return (
+    <NextLink
+      href={href}
+      className={`${styles.link} ${router.pathname === href && styles.active}`}
+      {...restProps}>
+      {children}
+    </NextLink>
+  );
+};
 export default Link;
