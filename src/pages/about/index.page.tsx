e4fa90b4b00a063e954cf4140b8146133eb57830
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2023-05-31 11:52:37 +0300

    feat: two blocks on aboutUs page

--- src/pages/about/index.page.tsx
+++ src/pages/about/index.page.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
+import AuthorsList from '@components/AuthorsList';
+import JoinUs from '@components/JoinUs';
+import Typography from '@components/Typography';
 import ContentContainer from '@containers/ContentContainer';
 
-const HomePage = (): JSX.Element => <ContentContainer>ABOUT</ContentContainer>;
+import styles from './about.module.scss';
+
+const HomePage = (): JSX.Element => {
+  const [t] = useTranslation();
+
+  return (
+    <div>
+      <ContentContainer className={styles.authors}>
+        <Typography variant="head2">{t('home.listOfAuthors')}</Typography>
+        <AuthorsList />
+      </ContentContainer>
+      <JoinUs />
+    </div>
+  );
+};
 
 export default HomePage;
