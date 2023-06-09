20497cb1b1a4eb133b917803def63306c778c142
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2023-06-09 17:00:45 +0300

    fix: fix author image on author page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -17,6 +17,7 @@ import routes from '@constants/routes';
 import testimonials from '@constants/testimonials';
 import ContentContainer from '@containers/ContentContainer';
 import { getFeaturedPost, getLastPost, getPagePosts } from '@services/posts';
+import useElementOnScreen from '@services/useElementOnScreen';
 
 import styles from './home.module.scss';
 
@@ -33,23 +34,15 @@ const HomePage = (): JSX.Element => {
 
   const [testimonialIndex, setTestimonalIndex] = useState(0);
 
+  const [elementRef, isOnScreen] = useElementOnScreen();
+
   const lastPost = getLastPost();
 
   const featuredPost = getFeaturedPost();
 
-  const postBlogRef = useRef<refType>(null);
-
-  const aboutUsRef = useRef<refType>(null);
-
-  const categoriesRef = useRef<refType>(null);
-
-  const whyWeStartedRef = useRef<refType>(null);
-
-  const authorsRef = useRef<refType>(null);
-
-  const testimonialsRef = useRef<refType>(null);
-
-  const contentRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    console.log('on screen');
+  }, [isOnScreen]);
 
   useEffect(() => {
     setDate(moment(lastPost.date).locale(i18n.language).format('MMM DD, YYYY'));
@@ -85,234 +78,218 @@ const HomePage = (): JSX.Element => {
 
   return (
     <div className={styles.page}>
-      <div ref={contentRef}>
-        <div className={styles.mainPost}>
-          <figure className={styles.mainPostImage}>
-            <Image
-              src={lastPost.image}
-              alt="Main"
-            />
-          </figure>
-          <ContentContainer className={styles.lastPost}>
-            <div className={styles.lastPostInfo}>
-              <Typography variant="body1">
-                {t('home.lastPost.postType')}
-              </Typography>
-              <Typography variant="head1">{lastPost.title}</Typography>
-              <Typography
-                className={styles.postInfo}
-                variant="body1">
-                By
-                <Typography
-                  className={styles.author}
-                  variant="body1">
-                  <Link href={`${routes.author}/${lastPost.author.id}`}>
-                    {lastPost.author.name}
-                  </Link>
-                </Typography>
-                | {dateFeatured}
-              </Typography>
-              <Typography
-                className={styles.postText}
-                variant="body1">
-                {lastPost.text[0][1]}
-              </Typography>
-              <Link href={`${routes.Blog}/${lastPost.id}`}>
-                <Button>
-                  <Typography variant="head5">
-                    {t('posts.readMore')} {'>'}
-                  </Typography>
-                </Button>
-              </Link>
-            </div>
-            <div className={styles.featuredImage} />
-          </ContentContainer>
-        </div>
-        <ContentContainer
-          ref={postBlogRef}
-          className={styles.postsBlock}>
-          <div className={styles.featuredBlock}>
-            <Typography variant="head2">{t('posts.featuredPost')}</Typography>
-            <div className={styles.featuredInfo}>
-              <figure className={styles.featuredImage}>
-                <Image
-                  src={featuredPost.image}
-                  alt="Featured"
-                />
-              </figure>
-              <Typography
-                className={styles.postInfo}
-                variant="body2">
-                By
-                <Link href={`${routes.author}/${featuredPost.author.id}`}>
-                  <Typography
-                    className={styles.author}
-                    variant="body2">
-                    {featuredPost.author.name}
-                  </Typography>
-                </Link>
-                | {date}
-              </Typography>
-              <Typography variant="head2">{featuredPost.title}</Typography>
-              <Typography
-                className={styles.postText}
-                variant="body1">
-                {featuredPost.text[0][1]}
-              </Typography>
-              <Link href={`${routes.Blog}/${featuredPost.id}`}>
-                <Button>
-                  <Typography variant="head5">
-                    {t('posts.readMore')} {'>'}
-                  </Typography>
-                </Button>
-              </Link>
-            </div>
-          </div>
-          <div className={styles.allPosts}>
-            <div className={styles.title}>
-              <Typography variant="head2">{t('posts.allPosts')}</Typography>
-              <Link href={routes.Blog}>
-                <Typography variant="body1">{t('posts.viewAll')}</Typography>
-              </Link>
-            </div>
-            <div className={styles.postsContainer}>
-              {getPagePosts(1).map((post) => (
-                <PostCard
-                  key={post.id}
-                  variant="small"
-                  post={post}
-                />
-              ))}
-            </div>
-          </div>
-        </ContentContainer>
-        <ContentContainer
-          ref={aboutUsRef}
-          className={styles.aboutUs}>
-          <div className={styles.blocks}>
-            <div>
-              <Typography variant="head6">{t('home.aboutUs.title')}</Typography>
-              <Typography variant="head2">
-                {t('home.aboutUs.ourDescriptionTitle')}
-              </Typography>
-              <Typography variant="body1">
-                {t('home.aboutUs.ourDescriptionText')}
-              </Typography>
-              <Link href={routes['About Us']}>
-                <Typography variant="head6">
-                  {t('posts.readMore')} {'>'}
-                </Typography>
-              </Link>
-            </div>
-
-            <div>
-              <Typography variant="head6">
-                {t('home.aboutUs.mission')}
-              </Typography>
-              <Typography variant="head3">
-                {t('home.aboutUs.missionTitle')}
-              </Typography>
-              <Typography variant="body1">
-                {t('home.aboutUs.missionText')}
-              </Typography>
-            </div>
-          </div>
-        </ContentContainer>
-        <ContentContainer
-          ref={categoriesRef}
-          className={styles.categories}>
-          <Typography variant="head1"> {t('home.chooseCategory')}</Typography>
-          <CategoriesList />
-        </ContentContainer>
-        <ContentContainer
-          ref={whyWeStartedRef}
-          className={styles.whyWeStartedBlock}>
-          <div className={styles.whyInfo}>
-            <Typography variant="head6">
-              {t('home.whyWeStarted.why')}
-            </Typography>
-            <Typography variant="head2">
-              {t('home.whyWeStarted.title')}
-            </Typography>
+      <div className={styles.mainPost}>
+        <figure className={styles.mainPostImage}>
+          <Image
+            src={lastPost.image}
+            alt="Main"
+          />
+        </figure>
+        <ContentContainer className={styles.lastPost}>
+          <div className={styles.lastPostInfo}>
             <Typography variant="body1">
-              {t('home.whyWeStarted.text')}
+              {t('home.lastPost.postType')}
             </Typography>
-            <Link href={`${routes['About Us']}`}>
+            <Typography variant="head1">{lastPost.title}</Typography>
+            <Typography
+              className={styles.postInfo}
+              variant="body1">
+              By
+              <Typography
+                className={styles.author}
+                variant="body1">
+                <Link href={`${routes.author}/${lastPost.author.id}`}>
+                  {lastPost.author.name}
+                </Link>
+              </Typography>
+              | {dateFeatured}
+            </Typography>
+            <Typography
+              className={styles.postText}
+              variant="body1">
+              {lastPost.text[0][1]}
+            </Typography>
+            <Link href={`${routes.Blog}/${lastPost.id}`}>
               <Button>
                 <Typography variant="head5">
-                  {t('home.whyWeStarted.button')} {'>'}
+                  {t('posts.readMore')} {'>'}
                 </Typography>
               </Button>
             </Link>
           </div>
-          <figure className={styles.teamImage}>
-            <Image
-              src={image}
-              alt="Team"
-            />
-          </figure>
+          <div className={styles.featuredImage} />
         </ContentContainer>
-        <ContentContainer
-          ref={authorsRef}
-          className={styles.authors}>
-          <Typography variant="head2">{t('home.listOfAuthors')}</Typography>
-          <AuthorsList />
-        </ContentContainer>
-        <ContentContainer ref={testimonialsRef}>
-          <div className={styles.testimonials}>
-            <div className={styles.constant}>
-              <Typography variant="head4">
-                {t('home.testimonials.name')}
+      </div>
+      <ContentContainer className={styles.postsBlock}>
+        <div className={styles.featuredBlock}>
+          <Typography variant="head2">{t('posts.featuredPost')}</Typography>
+          <div className={styles.featuredInfo}>
+            <figure className={styles.featuredImage}>
+              <Image
+                src={featuredPost.image}
+                alt="Featured"
+              />
+            </figure>
+            <Typography
+              className={styles.postInfo}
+              variant="body2">
+              By
+              <Link href={`${routes.author}/${featuredPost.author.id}`}>
+                <Typography
+                  className={styles.author}
+                  variant="body2">
+                  {featuredPost.author.name}
+                </Typography>
+              </Link>
+              | {date}
+            </Typography>
+            <Typography variant="head2">{featuredPost.title}</Typography>
+            <Typography
+              className={styles.postText}
+              variant="body1">
+              {featuredPost.text[0][1]}
+            </Typography>
+            <Link href={`${routes.Blog}/${featuredPost.id}`}>
+              <Button>
+                <Typography variant="head5">
+                  {t('posts.readMore')} {'>'}
+                </Typography>
+              </Button>
+            </Link>
+          </div>
+        </div>
+        <div className={styles.allPosts}>
+          <div className={styles.title}>
+            <Typography variant="head2">{t('posts.allPosts')}</Typography>
+            <Link href={routes.Blog}>
+              <Typography variant="body1">{t('posts.viewAll')}</Typography>
+            </Link>
+          </div>
+          <div className={styles.postsContainer}>
+            {getPagePosts(1).map((post) => (
+              <PostCard
+                key={post.id}
+                variant="small"
+                post={post}
+              />
+            ))}
+          </div>
+        </div>
+      </ContentContainer>
+      <ContentContainer className={styles.aboutUs}>
+        <div className={styles.blocks}>
+          <div>
+            <Typography variant="head6">{t('home.aboutUs.title')}</Typography>
+            <Typography variant="head2">
+              {t('home.aboutUs.ourDescriptionTitle')}
+            </Typography>
+            <Typography variant="body1">
+              {t('home.aboutUs.ourDescriptionText')}
+            </Typography>
+            <Link href={routes['About Us']}>
+              <Typography variant="head6">
+                {t('posts.readMore')} {'>'}
               </Typography>
-              <Typography variant="head2">
-                {t('home.testimonials.title')}
-              </Typography>
-              <Typography variant="body1">
-                {t('home.testimonials.text')}
-              </Typography>
-            </div>
-            <div className={styles.comment}>
+            </Link>
+          </div>
+
+          <div>
+            <Typography variant="head6">{t('home.aboutUs.mission')}</Typography>
+            <Typography variant="head3">
+              {t('home.aboutUs.missionTitle')}
+            </Typography>
+            <Typography variant="body1">
+              {t('home.aboutUs.missionText')}
+            </Typography>
+          </div>
+        </div>
+      </ContentContainer>
+      <ContentContainer className={styles.categories}>
+        <Typography variant="head1"> {t('home.chooseCategory')}</Typography>
+        <CategoriesList />
+      </ContentContainer>
+      <ContentContainer className={styles.whyWeStartedBlock}>
+        <div className={styles.whyInfo}>
+          <Typography variant="head6">{t('home.whyWeStarted.why')}</Typography>
+          <Typography variant="head2">
+            {t('home.whyWeStarted.title')}
+          </Typography>
+          <Typography variant="body1">{t('home.whyWeStarted.text')}</Typography>
+          <Link href={`${routes['About Us']}`}>
+            <Button>
               <Typography variant="head5">
-                {testimonials[testimonialIndex].text}
+                {t('home.whyWeStarted.button')} {'>'}
               </Typography>
-              <div className={styles.lowBlock}>
-                <div className={styles.user}>
-                  <Image
-                    src={testimonials[testimonialIndex].image}
-                    alt="Avatar"
-                  />
-                  <div className={styles.userInfo}>
-                    <Typography variant="head5">
-                      {testimonials[testimonialIndex].author}
-                    </Typography>
-                    <Typography variant="body1">
-                      {testimonials[testimonialIndex].place}
-                    </Typography>
-                  </div>
+            </Button>
+          </Link>
+        </div>
+        <figure className={styles.teamImage}>
+          <Image
+            src={image}
+            alt="Team"
+          />
+        </figure>
+      </ContentContainer>
+      <ContentContainer className={styles.authors}>
+        <Typography variant="head2">{t('home.listOfAuthors')}</Typography>
+        <AuthorsList />
+      </ContentContainer>
+      <ContentContainer>
+        <div
+          className={styles.testimonials}
+          ref={elementRef}>
+          <div className={styles.constant}>
+            <Typography variant="head4">
+              {t('home.testimonials.name')}
+            </Typography>
+            <Typography variant="head2">
+              {t('home.testimonials.title')}
+            </Typography>
+            <Typography variant="body1">
+              {t('home.testimonials.text')}
+            </Typography>
+          </div>
+          <div className={styles.comment}>
+            <Typography variant="head5">
+              {testimonials[testimonialIndex].text}
+            </Typography>
+            <div className={styles.lowBlock}>
+              <div className={styles.user}>
+                <Image
+                  src={testimonials[testimonialIndex].image}
+                  alt="Avatar"
+                />
+                <div className={styles.userInfo}>
+                  <Typography variant="head5">
+                    {testimonials[testimonialIndex].author}
+                  </Typography>
+                  <Typography variant="body1">
+                    {testimonials[testimonialIndex].place}
+                  </Typography>
                 </div>
-                <div className={styles.control}>
-                  <div
-                    className={styles.back}
-                    onClick={prevTestimonal}>
-                    <Image
-                      src={arrow}
-                      alt="arrow"
-                    />
-                  </div>
-                  <div
-                    className={styles.next}
-                    onClick={nextTestimonal}>
-                    <Image
-                      src={arrow}
-                      alt="arrow"
-                    />
-                  </div>
+              </div>
+              <div className={styles.control}>
+                <div
+                  className={styles.back}
+                  onClick={prevTestimonal}>
+                  <Image
+                    src={arrow}
+                    alt="arrow"
+                  />
+                </div>
+                <div
+                  className={styles.next}
+                  onClick={nextTestimonal}>
+                  <Image
+                    src={arrow}
+                    alt="arrow"
+                  />
                 </div>
               </div>
             </div>
           </div>
-        </ContentContainer>
-      </div>
+        </div>
+      </ContentContainer>
       <JoinUs />
     </div>
   );
